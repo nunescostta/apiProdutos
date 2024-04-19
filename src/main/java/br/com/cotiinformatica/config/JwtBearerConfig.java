d4232ac91ce6a6f59925455f6b7430e09eac00b9
nunescostta <jeanrodrigo1324@gmail.com>
2024-04-19 09:58:25 -0300

    Finalização Api controle de Produtos

---
+++ src/main/java/br/com/cotiinformatica/config/JwtBearerConfig.java
@@ -0,0 +1,22 @@
+package br.com.cotiinformatica.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import br.com.cotiinformatica.filters.AuthenticationFilter;
+
+@Configuration
+public class JwtBearerConfig {
+	@Bean
+	public FilterRegistrationBean<AuthenticationFilter> jwtFilter() {
+
+		// registrando o filter que irá validar os tokens
+		FilterRegistrationBean<AuthenticationFilter> filter = new FilterRegistrationBean<AuthenticationFilter>();
+
+		filter.setFilter(new AuthenticationFilter());
+
+		// mapeando toda a API para exigir o token de autenticação
+		filter.addUrlPatterns("/api/*");
+		return filter;
+	}
+}
